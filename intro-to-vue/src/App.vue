720043f5373f3f1d2278e5a35a0aa0be6c08fc18
mrtony <tonychenn@gmail.com>
2019-02-24 13:47:42 +0800

    [Intro To Vue][List Redering][using v-for to finish example]

--- intro-to-vue/src/App.vue
+++ intro-to-vue/src/App.vue
@@ -10,8 +10,21 @@
         <p v-if="inventory > 10">In Stocks</p>
         <p v-else-if="inventory <= 10 && inventory > 0">Almost dsold out!</p>
         <p v-if="inventory === 0">Sold Out!</p>
+
+        <ul>
+          <li v-for="detail in details" :key="detail">
+            {{detail}}
+          </li>
+        </ul>
+
+        <div>
+          <p v-for="variant in variants" :key="variant.variantId">
+            {{variant.variantColor}}
+          </p>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -21,7 +34,12 @@ export default {
   data: () => ({
     product: 'Socks',
     image: require("./assets/vmSocks-green-onWhite.jpg"),
-    inventory: 0
+    inventory: 0,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {variantId: 234, variantColor: 'green'},
+      {variantId: 235, variantColor: 'blue'},
+    ]
   })
 }
 </script>
